3354d5c81c79b9ee0cecbe9cb3e609e99098324a
Hoàng Dũng <dungbui8198@gmail.com>
2024-03-08 04:16:23 +0700

    entities, repositories, filter

---
+++ src/main/java/com/sof3011/assignment/security/AuthFilter.java
@@ -0,0 +1,17 @@
+package com.sof3011.assignment.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+
+import java.io.IOException;
+@WebFilter({})
+public class AuthFilter extends HttpFilter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        super.doFilter(req, res, chain);
+    }
+}
