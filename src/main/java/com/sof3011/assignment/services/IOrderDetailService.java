cfe20a7f12af36ca06bcf9bf1225ed372d3bc450
Hoàng Dũng <dungbui8198@gmail.com>
2024-03-27 03:14:08 +0700

    checkout function

--- src/main/java/com/sof3011/assignment/services/IOrderDetailService.java
+++ src/main/java/com/sof3011/assignment/services/IOrderDetailService.java
@@ -1,9 +1,10 @@
 package com.sof3011.assignment.services;
 
+import com.sof3011.assignment.entities.Cart;
 import com.sof3011.assignment.entities.OrderDetail;
 
 import java.util.List;
 
 public interface IOrderDetaiService extends IServiceInterface<OrderDetail,Long> {
-    OrderDetail createOrder(List<Long> idOderItems);
+    OrderDetail createOrder(List<Cart> cartItems, OrderDetail orderDetail);
 }
